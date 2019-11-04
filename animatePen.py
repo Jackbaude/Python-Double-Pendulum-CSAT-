915df3b56af53a98319b977f9797ff043824d42c
Jack <Jackbaude@gmail.com>
2019-11-03 19:19:59 -0600

    added a tracing line of the path of the tail

--- animatePen.py
+++ animatePen.py
@@ -10,34 +10,49 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 from doublePen import DoublePen
 
-#the pendulum object
-p1 = DoublePen(1, 1, 1, 1, 180.0, 16.0)
+# the pendulum object
+p1 = DoublePen(1, 1, 1, 1, 180.0, 270.0)
 
 
 fig, ax = plt.subplots()
+plt.axis("off")
 xdata, ydata = [], []
-ax.grid()
-(ln,) = plt.plot([], [],'-o',alpha=1)
+mass_two_xs = []
+mass_two_ys = []
+# animating the double pendulum arm
+(ln,) = plt.plot([], [], "-o", alpha=1)
+# animating the second path of the pendulum or the path of the tail
+(mass_two,) = plt.plot([], [], lw=2, alpha=0.5)
 
-#the init for the animation FuncAnimation
+# the init for the animation FuncAnimation
 def init():
     ax.set_xlim(-(p1.l1 + p1.l2), (p1.l1 + p1.l2))
     ax.set_ylim(-(p1.l1 + p1.l2), (p1.l1 + p1.l2))
-    return (ln,)
 
-#runs every frame that is made
+    return (ln, mass_two)
+
+
+# runs every frame that is made
 def update(frame):
     points = p1.calculate((frame))
     xs = points[0]
     ys = points[1]
+    # finds the cords of the second mass so we can trace them
+    mass_two_x = points[0][2]
+    mass_two_y = points[1][2]
     xdata.clear()
     ydata.clear()
+    # add the cords of mass 2 to an array so we can graph them
+    mass_two_xs.append(mass_two_x)
+    mass_two_ys.append(mass_two_y)
     xdata.append(xs)
     ydata.append(ys)
     ln.set_data(xdata, ydata)
-    return (ln,)
+    mass_two.set_data(mass_two_xs, mass_two_ys)
+    return (ln, mass_two)
 
-#animates the double pendulum
+
+# animates the double pendulum
 ani = FuncAnimation(
     fig,
     update,
@@ -46,7 +61,7 @@ ani = FuncAnimation(
     init_func=init,
     blit=True,
 )
-#saves the animation to double_pendulum.mp4, with 60fps
-ani.save('double_pendulum.mp4', fps=60,)
-#plays the animation afterword through matplotlib
+# saves the animation to double_pendulum.mp4, with 60fps
+ani.save("double_pendulum_trace3.mp4", fps=60)
+# plays the animation afterword through matplotlib
 plt.show()
